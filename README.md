86e91f6055f7bdec786f3eaca7169e1e37fd221e
Brandon Barnes <winterknight@email.com>
2017-07-24 10:30:30 -0700

    Second Commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# wiimc-youtube
---
+++ youtube.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import cgi
+form = cgi.FieldStorage()
+
+import re
+import httplib
+import urllib
+import subprocess
+import sys
+import os
+
+if ("query" not in form and "watch" not in form):
+	print("Content-type: text/plain\n")
+	print("No search query entered.")
+	exit
+
+if ("watch" in form):
+	watch = str(form["watch"].value)
+	outfile = "/var/www/media/" + watch + ".mp4"
+	subprocess.call(["youtube-dl", "-f", "18", "-o", outfile, "https://www.youtube.com/watch?v=" + watch], stdout = open("/dev/null", "w"))
+	print("Content-type: video/mp4")
+	print("Content-Length: " + str(os.path.getsize(outfile)) + "\n")
+	f = open(outfile)
+	while (True):
+		chunk = f.read(1048576)
+		if (len(chunk) == 0):
+			f.close()
+			break
+		sys.stdout.write(chunk)
+	exit
+
+
+query = str(form["query"].value)
+
+conn = httplib.HTTPSConnection("www.youtube.com")
+conn.request("GET", "/results?search_query=" + urllib.quote(query))
+res = conn.getresponse()
+ytlines = res.read().split("\n")
+conn.close()
+
+names = []
+urls = []
+for i in ytlines:
+	m = re.search('/watch\?v=[^"]*"[^>]*title="[^"]*"', i)
+	if (m):
+		if (re.search('ypc-badge', i)):
+			continue
+		m = i[m.start():m.end()]
+		names.append(re.sub('/watch\?v=([^"]*)".*title="([^"]*)".*', r'\2', m))
+		urls.append(re.sub('/watch\?v=([^"]*)".*title="([^"]*)".*', r'\1', m))
+
+print("Content-type: text/plain\n")
+for i,j in zip(names, urls):
+	print("youtube.py?watch=" + j + "&videoname=/" + i)
